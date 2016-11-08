44bfb05f44e0523467411366fd7b838036ef6471
christennguyen <christennguyen@Christens-MacBook-Pro.local>
2016-11-08 12:41:50 -0800

    Judge form JS Selectors added
    Added selectors for judge_form.js and made minor changes in the
    judgeevals.html

--- js/judge_form.js
+++ js/judge_form.js
@@ -2,6 +2,13 @@ $(function() {
     var mockGet = { code: "12345",
                     firstname: "Abe",
                     lastname: "Millan" };
+
+    //holding off on this until login is implemented -- need to know form input names
+    //var newGet = { code: document.getElementsByName('code')[0].value
+    //               firstname: document.getElementsByName('firstname')[0].value
+    //               lastname: document.getElementsByName('lastname')[0].value       
+    //}
+
     $.get('server/judge_form.php', mockGet, function(data) {
         console.log('Data Loading Get');
         console.log(data);
@@ -13,7 +20,7 @@ $(function() {
     // clicked and each time check whether they are all selected
     // If they are all then you need to make the submit button clickable
     //
-    // This will also hagve to be checked by the server-side code since some one
+    // This will also have to be checked by the server-side code since some one
     // can modify the thing using a web tool
 //    if ( $( ".required" ).val().length === 0 ) {
          
@@ -26,6 +33,7 @@ $(function() {
                                   "firstname": "Abe",
                                    "lastname": "Millan", 
                                "techaccuracy": "1",
+                                 "creativity": "4",
                                  "analytical": "2",
                                  "methodical": "3", 
                                  "complexity": "4",
@@ -38,9 +46,28 @@ $(function() {
                                  "confidence": "3", 
                                       "total": "2", 
                                     "comment": "Good Job"}] };
+
+        var formData = { data: [{"project_id": "1", 
+                                  "firstname": document.getElementsByName('firstname')[0].value,
+                                   "lastname": document.getElementsByName('lastname')[0].value, 
+                               "techaccuracy": document.querySelector('input[name="techaccuracy"]:checked').value,
+                                 "creativity": document.querySelector('input[name="techaccuracy"]:checked').value,
+                                 "analytical": document.querySelector('input[name="techaccuracy"]:checked').value,
+                                 "methodical": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                 "complexity": document.querySelector('input[name="techaccuracy"]:checked').value,
+                                 "completion": document.querySelector('input[name="techaccuracy"]:checked').value,
+                                     "design": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                      "qanda": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                               "organization": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                       "time": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                    "visuals": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                 "confidence": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                      "total": document.querySelector('input[name="techaccuracy"]:checked').value, 
+                                    "comment": document.getElementsByName('comment')[0].value}] };
+                                                                
         formURL = $( "#projectEval" ).attr('action');
          
-        $.post(formURL, mockData, function(data, textStatus, jqXHR) {
+        $.post(formURL, formData, function(data, textStatus, jqXHR) {
             console.log('Judge PHP return');
             console.log(data);
             $( "#projectEval" ).hide();
@@ -53,7 +80,7 @@ $(function() {
             console.log('Judge PHP Error');
             console.log(errrorThrown);
         }); 
-    // Now check whether the radio buttons are numbers
+    // Now check whether the radio buttons are numbers -- not necessary since all radio buttons are checked by default
     // Check that the comment is sthring and not mallicious
     
 
